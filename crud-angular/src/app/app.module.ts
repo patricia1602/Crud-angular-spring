67b85383b14d891118cc4d74ca0f899a5d9e1a22
Patricia <patyamiga16@hotmail.com>
2022-09-19 11:30:13 -0300

    aula4 correcao

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
