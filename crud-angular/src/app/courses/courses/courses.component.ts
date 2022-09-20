53102d101566eb12547750064cbde2141f74b452
Patricia <patyamiga16@hotmail.com>
2022-09-20 09:51:43 -0300

    aula7

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -9,7 +9,7 @@ import { Course } from '../model/course';
 export class CoursesComponent implements OnInit {
 
   courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
+    { _id: '1', name: 'Angular', category: 'front-end' }
   ];
   displayedColumns = ['name', 'category'];
 
