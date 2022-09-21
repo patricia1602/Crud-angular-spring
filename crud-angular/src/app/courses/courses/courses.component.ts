132fa107f61cd26a0283ec9c52d35563e2d154b4
Patricia <patyamiga16@hotmail.com>
2022-09-21 10:34:57 -0300

    aula10

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -10,7 +10,7 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-    courses: Observable<Course[]>;
+    courses$: Observable<Course[]>;
   //courses: Course[] = [];
 
   displayedColumns = ['name', 'category'];
@@ -20,7 +20,7 @@ export class CoursesComponent implements OnInit {
   constructor(private coursesService : CoursesService) {
     //this.courses= [];
     //this.coursesService= new CoursesService ();
-      this.courses = this.coursesService.list();
+      this.courses$ = this.coursesService.list();
 
    //this.coursesService.list().subscribe(courses => this.courses = courses);
    }
