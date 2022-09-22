bf9fd339a50c0bf3d69d03fe110d145f57d6a82f
Patricia <patyamiga16@hotmail.com>
2022-09-22 17:48:42 -0300

    aula 14- add projeto spring

--- crud-spring/src/main/java/com/leona/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/leona/crudspring/CrudSpringApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudSpringApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
