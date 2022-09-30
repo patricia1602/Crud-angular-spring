2cfaacdc24eb8a4982bbbe82e6b9042302a1665b
Patricia <patyamiga16@hotmail.com>
2022-09-30 15:32:36 -0300

    aula20

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, first, take, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 import { Course } from '../model/course';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +20,9 @@ export class CoursesService {
       //delay(5000),
        tap(courses => console.log(courses))
       );
+  }
+
+  save(record: Course) {
+     return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 }
--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-    exports: [
+  exports: [
     MatCardModule,
     MatTableModule,
     MatToolbarModule,
@@ -19,7 +21,9 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
   ],
 })
-export class AppMaterialModule { }
+export class AppMaterialModule {}
