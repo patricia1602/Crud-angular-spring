8ab37500e796ee729c7ea17ec0d9f1f291715f93
Patricia <patyamiga16@hotmail.com>
2022-09-30 17:53:10 -0300

    aula21

--- crud-spring/src/main/java/com/leona/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/leona/crudspring/controller/CourseController.java
@@ -2,9 +2,13 @@ package com.leona.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leona.crudspring.model.Course;
@@ -23,6 +27,16 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        //System.out.println(course.getName());
+          return courseRepository.save(course);
+        //return ResponseEntity.status(HttpStatus.CREATED)
+        //.body(courseRepository.save(course));
     }
  
 }
